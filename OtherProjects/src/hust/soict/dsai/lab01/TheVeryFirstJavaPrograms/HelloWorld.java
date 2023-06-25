410c416371644f378a04dcd256c85007053223ed
Anh Cong Phan <88734380+pycank@users.noreply.github.com>
2023-06-25 23:55:27 +0700

    Delete OtherProjects directory

--- OtherProjects/src/hust/soict/dsai/lab01/TheVeryFirstJavaPrograms/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package hust.soict.dsai.lab01.TheVeryFirstJavaPrograms;
-
-// Exercise 2.2.1
-public class HelloWorld {
-    public static void main(String args[]) {
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t world!");
-    }
-}
