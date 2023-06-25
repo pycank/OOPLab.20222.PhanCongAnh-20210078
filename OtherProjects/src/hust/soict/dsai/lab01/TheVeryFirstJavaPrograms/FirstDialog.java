bb4bf95997e7f7e3f737656b7fbaad11acadca52
Anh Cong Phan <88734380+pycank@users.noreply.github.com>
2023-06-25 23:54:42 +0700

    Add files via upload

---
+++ OtherProjects/src/hust/soict/dsai/lab01/TheVeryFirstJavaPrograms/FirstDialog.java
@@ -0,0 +1,10 @@
+package hust.soict.dsai.lab01.TheVeryFirstJavaPrograms;
+
+import javax.swing.JOptionPane;
+// Exercise 2.2.2
+public class FirstDialog{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
